be23f7026ce927fca77d96a7fb8e68722cb731e1
aloysiuslip <aloysius.lip@gmail.com>
2020-04-01 03:42:46 +0100

    Updates

---
+++ precache-manifest.d61d65e2d9b807b034ded0b089eea790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961bb55bb1ab626da6e55fe5623b16c0",
+    "url": "/toastie-splash/index.html"
+  },
+  {
+    "revision": "9e0b788ffa5648d5d664",
+    "url": "/toastie-splash/static/css/main.d3a94a4f.chunk.css"
+  },
+  {
+    "revision": "97b9c404f1657f86366d",
+    "url": "/toastie-splash/static/js/2.be92706b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toastie-splash/static/js/2.be92706b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0b788ffa5648d5d664",
+    "url": "/toastie-splash/static/js/main.d1f5bde5.chunk.js"
+  },
+  {
+    "revision": "bef59675189cc86211ef",
+    "url": "/toastie-splash/static/js/runtime-main.f297e5ac.js"
+  }
+]);
